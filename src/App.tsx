ed15ec32de6783e1d2d3799ec6b01d84d6411b67
kkatalz <zlata.kiev2004@gmail.com>
2026-04-30 12:22:13 +0300

    feat: add a NavBar icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import './App.css';
+import NavBar from '@/components/NavBar';
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area='nav'></GridItem>
+      <GridItem area='nav'>
+        <NavBar />
+      </GridItem>
       <GridItem area='aside' bg='gold' display={{ base: 'none', lg: 'block' }}>
         Aside
       </GridItem>
