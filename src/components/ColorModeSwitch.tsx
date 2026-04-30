ed8c562f0f88db3a4828a7787bd154d1fde20db6
kkatalz <zlata.kiev2004@gmail.com>
2026-04-30 12:55:03 +0300

    feat: remove default styles; add a color switch for light and dark mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,24 @@
+import { useColorMode } from '@/components/ui/color-mode';
+import { HStack, Switch, Text } from '@chakra-ui/react';
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch.Root
+        checked={colorMode === 'dark'}
+        onCheckedChange={toggleColorMode}
+      >
+        <Switch.HiddenInput />
+        <Switch.Control>
+          <Switch.Thumb />
+        </Switch.Control>
+      </Switch.Root>
+
+      <Text>{colorMode === 'light' ? 'Light Mode' : 'Dark Mode'}</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from '@/components/ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
       <Image src={logo} boxSize='60px' />
-      <Text fontSize='2xl' fontWeight='bold'>
-        GameHub
-      </Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
