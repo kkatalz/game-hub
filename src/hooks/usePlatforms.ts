f45750c544ccd36513933f538308a0e461b4c360
kkatalz <zlata.kiev2004@gmail.com>
2026-05-13 16:21:47 +0300

    clean: comment out unused interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,10 @@
 import platforms from '../components/data/platforms';
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 // const usePlatforms = () => useData<Platform>('/platforms/lists/parents');  Do not call the API. Return dummy data instead.
 const usePlatforms = () => ({ data: platforms, error: null });
