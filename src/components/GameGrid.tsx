318b119e26acd9b3c78e7076cd02d6ed48fd56d3
kkatalz <zlata.kiev2004@gmail.com>
2026-05-13 16:38:28 +0300

    feat: deploy to vervel; add margin-left for Network error

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text ml={2}>{error}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
