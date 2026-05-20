5d6683c8bcc6e17bf13c2e75223a056a683d33d8
kkatalz <zlata.kiev2004@gmail.com>
2026-05-20 18:41:53 +0300

    feat: fetch platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,30 @@
-import platforms from '../components/data/platforms';
+import { useQuery } from '@tanstack/react-query';
+import type { FetchResponse } from '@/hooks/useData';
+import apiClient from '@/services/api-client';
+import platforms from '@/components/data/platforms';
 
-// interface Platform {
-//   id: number;
-//   name: string;
-//   slug: string;
-// }
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 // const usePlatforms = () => useData<Platform>('/platforms/lists/parents');  Do not call the API. Return dummy data instead.
-const usePlatforms = () => ({ data: platforms, error: null });
+// const usePlatforms = () => ({ data: platforms, error: null });
+
+const usePlatforms = () => {
+  const fetchPlatforms = async () => {
+    return await apiClient
+      .get<FetchResponse<Platform>>('/platforms/lists/parents')
+      .then((response) => response.data.results);
+  };
+
+  return useQuery<Platform[], Error>({
+    queryKey: ['platforms'],
+    queryFn: fetchPlatforms,
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: platforms,
+  });
+};
 
 export default usePlatforms;
