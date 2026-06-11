913dd57403071e839bd0eee827acf5f77aeb7005
kkatalz <zlata.kiev2004@gmail.com>
2026-06-11 18:47:48 +0300

    fix: the navbar logic

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from '@/components/ColorModeSwitch';
 import SearchInput from '@/components/SearchInput';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <HStack padding='10px' gap='1rem'>
-      <Image src={logo} boxSize='60px' />
+      <Image src={logo} boxSize='60px' cursor='pointer' onClick={() => navigate('/')} />
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
